67df8121028ef97990010e8cdc9dc1442b41a2c7
Thomas Vigouroux <thomas.vigouroux@protonmail.com>
2022-08-23 20:03:48 +0200

    ci: parse examples

---
+++ scripts/parse_runtime_files.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+BASE_URL="https://raw.githubusercontent.com/neovim/neovim"
+OUT_PATH="/tmp/rt_file.vim"
+
+FILES="pack/dist/opt/matchit/autoload/matchit.vim"
+
+for file in "$FILES"
+do
+  echo "Doing $file"
+  wget -O "$OUT_PATH" "$BASE_URL/$NVIM_VERSION/runtime/$file"
+  tree-sitter parse --quiet "$OUT_PATH" || exit 1
+done
